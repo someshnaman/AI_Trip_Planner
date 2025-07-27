f4434d3bdde51289b77697f7713936ce01b0f67a
someshnaman <namanayo5@gmail.comgit config --global user.name someshnamangit config --global user.email namanayo5@gmail.com>
2025-07-27 21:37:54 +0530

    Travily search added

---
+++ utils/place_info_search.py
@@ -0,0 +1,50 @@
+import json
+from langchain_tavily import TavilySearch
+import os
+
+class TavilySearchTool:
+    """
+    Tool for searching attractions, restaurants, activities, or transportation using TavilySearch.
+    """
+    def __init__(self):
+        pass
+
+    def tavily_search_attractions(self, place)-> dict:
+        """
+        Searches for attractions in the specified place using TavilySearch.
+        """
+        tavily_tool = TavilySearch(topic='general', include_answer='advanced')
+        result = tavily_tool.invoke({"query": f"top attractive places in and around {place}"})
+        if isinstance(result, dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+    def tavily_search_restaurants(self, place: str) -> dict:
+        """
+        Searches for available restaurants in the specified place using TavilySearch.
+        """
+        tavily_tool = TavilySearch(topic="general", include_answer="advanced")
+        result = tavily_tool.invoke({"query": f"what are the top 10 restaurants and eateries in and around {place}."})
+        if isinstance(result, dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+    def tavily_search_activity(self, place: str) -> dict:
+        """
+        Searches for popular activities in the specified place using TavilySearch.
+        """
+        tavily_tool = TavilySearch(topic="general", include_answer="advanced")
+        result = tavily_tool.invoke({"query": f"activities in and around {place}"})
+        if isinstance(result, dict) and result.get("answer"):
+            return result["answer"]
+        return result
+
+    def tavily_search_transportation(self, place: str) -> dict:
+        """
+        Searches for available modes of transportation in the specified place using TavilySearch.
+        """
+        tavily_tool = TavilySearch(topic="general", include_answer="advanced")
+        result = tavily_tool.invoke({"query": f"What are the different modes of transportations available in {place}"})
+        if isinstance(result, dict) and result.get("answer"):
+            return result["answer"]
+        return result
